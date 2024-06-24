39678c5e4a3b4ad07db153c219e7b296d13ffbd6
Eduardo Cales Ferreira <dudu.calesferreira@gmail.com>
2024-06-23 23:31:52 -0300

    fazendo a divisao das paginas

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from bibliotech.views import home, sobre, contato
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- bibliotech/views.py
+++ bibliotech/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'bibliotech/home.html')
+    return render(request, 'bibliotech/pages/home.html')
